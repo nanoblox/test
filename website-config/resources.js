46050f11cf636d11159270956c3cfa5e96f640b2
Ben Horton <51117782+1ForeverHD@users.noreply.github.com>
2021-08-10 13:38:59 +0100

    Create resources.js

---
+++ website-config/resources.js
@@ -0,0 +1,5 @@
+_id: ObjectId("611154594dc05daf5afa666c")
+title: "pineapple"
+description: "does not go on pizza"
+imageUrl: "https://i.imgur.com/VFSTQhd.png"
+resourceUrl: "https://thspublications.com/opinion/2019/04/29/pineapple-pizza-a-disgr..."
